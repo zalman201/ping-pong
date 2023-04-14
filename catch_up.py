68a192d1bbb3c30cecbe8995ca272065b6a8c6e2
zalman201 <129939635+zalman201@users.noreply.github.com>
2023-04-14 11:59:27 +0300

    added platforms and their movements

--- catch_up.py
+++ catch_up.py
@@ -3,9 +3,32 @@ from pygame import *
 from random import *
 # классы GameSprite и Player
 class GameSprite(sprite.Sprite):
-    pass
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect() # хитбокс спрайта
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
-    pass
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+
 
 
 # описание игровой сцены, флаги состояний игры, надписи
@@ -17,6 +40,8 @@ background = transform.scale(image.load('oblaka.jpg'), (800,600))
 game = True
 
 # создание игровых спрайтов
+racket1 = Player('racket.png', 30, 200, 100, 250, 4)
+racket2 = Player('racket.png', 700, 200, 100, 250, 4)
 
 # пока игра продолжается
 while game:
@@ -30,149 +55,12 @@ while game:
 ###### Закрыть приложение
 
 ### Переместить ракетки
+    racket1.update_l()
+    racket2.update_r()
+
+    racket1.reset()
+    racket2.reset()
 
 ### Обновить спрайты и сцену
     display.update()
     clock.tick(40)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from pygame import *
-# window = display.set_mode((700,500))
-# clock = time.Clock()
-# display.set_caption("Догонялки")
-# background = transform.scale(image.load("background.png"), (700,500))
-
-
-
-# game = True
-# FPS = 60
-
-# sprite1 = transform.scale(image.load("sprite1.png"), (100,100))
-# sprite2 = transform.scale(image.load("sprite2.png"), (100,100))
-
-# x1 = 100
-# y1 = 300
-
-# x2 = 300
-# y2 = 300
-
-# speed = 10
-
-
-# while game:
-#     window.blit(background,(0, 0))
-#     window.blit(sprite1, (x1,y1))
-#     window.blit(sprite2, (x2,y2))
-
-
-#     for e in event.get():
-#         if e.type == QUIT:
-#             game = False
-
-#     keys_pressed = key.get_pressed()
-
-
-#     if keys_pressed[K_LEFT] and x1 > 5:
-#         x1 -= speed
-#     if keys_pressed[K_RIGHT] and x1 < 595:
-#         x1 += speed
-#     if keys_pressed[K_UP] and y1 > 5:
-#         y1 -= speed
-#     if keys_pressed[K_DOWN] and y1 < 395:
-#         y1 += speed
-
-#     display.update()
-#     clock.tick(FPS)
-
-
-# #создай окно игры
-
-# #задай фон сцены
-
-# #создай 2 спрайта и размести их на сцене
-
-# #обработай событие «клик по кнопке "Закрыть окно"»
